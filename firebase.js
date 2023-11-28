38dea96e9ba2bb07fdb550247db539097dc26241
Sai Nyi Nyi Tun <mgsainyinyitun.tumdy@gmail.com>
2023-11-28 11:56:47 +0630

    Added .env to .gitignore

--- firebase.js
+++ firebase.js
@@ -1,15 +1,23 @@
 import { initializeApp } from 'firebase/app';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('apikey=',process.env.APIKEY)
+console.log('authDomain=',process.env.AUTHDOMAIN)
+console.log('projectId=',process.env.PROJECTID)
+console.log('storageBucket=',process.env.STORAGEBUCKET)
+console.log('messagingSenderId=',process.env.MESSAGINGSENDID)
+console.log('appId=',process.env.APPID)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDCscseo9IwLvc_Fm_kBOwXsDpXdORgoSA",
-  authDomain: "sai-rest-api.firebaseapp.com",
-  projectId: "sai-rest-api",
-  storageBucket: "sai-rest-api.appspot.com",
-  messagingSenderId: "66401358249",
-  appId: "1:66401358249:web:0f5d6e6a956279678934b8"
+  apiKey:process.env.APIKEY,
+  authDomain:process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId:process.env.MESSAGINGSENDID,
+  appId:process.env.APPID
 };
 
-
-
 const firebase = initializeApp(firebaseConfig);
-
 export default firebase;
