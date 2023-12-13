a20adc11f46bd2ce2697123946690a11fdef7b56
mgsainyinyitun <mgsainyinyitun.tumdy@gmail.com>
2023-12-13 09:49:16 +0630

    json isoDateString to formatted date String function added

--- models/tkberror/TKBError.js
+++ models/tkberror/TKBError.js
@@ -9,12 +9,23 @@ class TKBError {
         this.message = message;
     }
     
+
+     convertISOToFormattedString(isoString) {
+        const date = new Date(isoString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+        return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+    }
+
     getErrorId() {
         return this.id;
     }
 
     display() {
-
         const tkb = {
             occureDate: this.occureDate,
             group: this.group,
@@ -33,7 +44,7 @@ class TKBError {
     getTKBError() {
         return ({
             id: this.id,
-            occureDate: this.occureDate,
+            occureDate: this.convertISOToFormattedString(this.occureDate),
             group: this.group,
             server: this.server,
             serverity: this.serverity,
